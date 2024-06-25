8c89094a5d52b4d8f6f26a19cadb0b39ac7e196e
IwinosaEkhator <brasenekhator@gmail.com>
2024-06-25 22:36:04 +0100

    Just arranging the controllers

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\AboutPageController;
 use App\Http\Controllers\AdminPageController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ContactPageController;
-use App\Http\Controllers\LibraryPageController;
-use App\Http\Controllers\MyHubPageController;
-use App\Http\Controllers\RegisterPageController;
-use App\Http\Controllers\SignInPageController;
-use App\Http\Controllers\StudyHubPageController;
-use App\Http\Controllers\UploadPageController;
 use App\Http\Controllers\UserPageController;
 use Illuminate\Support\Facades\Route;
 
