2cdb558ec6262660553d1ce4e856201aae065793
IwinosaEkhator <brasenekhator@gmail.com>
2024-06-25 21:16:14 +0100

    Updated Message

--- app/Http/Controllers/UserPageController.php
+++ app/Http/Controllers/UserPageController.php
@@ -6,8 +6,27 @@ use Illuminate\Http\Request;
 
 class UserPageController extends Controller
 {
-    public function index()
+
+    // Home
+
+    public function home()
     {
         return view('index');
     }
+
+    // Library
+
+    public function index()
+    {
+        return view('library');
+    }
+
+    // Contact Us
+    
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    
 }
